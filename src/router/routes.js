bca387d8ed2a62d7d3cfc536ff00a04bd1c097d1
Fabiola Flores <allisonchaconflores@gmail.com>
2025-03-06 12:24:02 -0600

    placa

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,9 +8,8 @@ const routes = [
       { path: '/prueba2/src/pages/MiLector.vue', component: () => import('src/pages/MiLector.vue') }, // Ruta para MiSegundo.vue
       { path: '/prueba2/src/pages/TablaLibros.vue', component: () => import('src/pages/TablaLibros.vue') }, // Ruta para TablaLibros.vue
 
-      { path: '/prueba2/src/pages/prueba1.vue', component: () => import('src/pages/prueba1.vue') }, // Ruta para TablaLibros.vue
-
     ],
+
   },
 
   // Siempre deja esta última ruta para manejar rutas no encontradas (404)
