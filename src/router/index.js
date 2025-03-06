6b179d380aac477a8dd5d82ddfbce28a6aa523d3
Fabiola Flores <allisonchaconflores@gmail.com>
2025-03-06 10:55:34 -0600

    Añadir las librerias

--- src/router/index.js
+++ src/router/index.js
@@ -2,34 +2,6 @@ import { defineRouter } from '#q-app/wrappers'
 import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
 import routes from './routes'
 
-/*AGREGADO*/
-import { register } from 'register-service-worker';
-
-if (process.env.PROD) {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
-      console.log('La app está siendo servida desde la caché por un service worker.');
-    },
-    registered() {
-      console.log('Service worker ha sido registrado.');
-    },
-    cached() {
-      console.log('El contenido ha sido cacheado para uso offline.');
-    },
-    updatefound() {
-      console.log('Nuevo contenido está siendo descargado.');
-    },
-    updated() {
-      console.log('Nuevo contenido está disponible; por favor, recarga.');
-    },
-    offline() {
-      console.log('No se encontró conexión a Internet. La app se está ejecutando en modo offline.');
-    },
-    error(error) {
-      console.error('Error durante el registro del service worker:', error);
-    },
-  });
-}
 
 /*
  * If not building with SSR mode, you can
