6a2d5c4504c7d7839cb80fdd59c0ce3240bd1faf
AllisonChacon1207 <allisonchaconflores@gmail.com>
2025-05-19 07:49:15 -0300

    Arreglado el error 404 en la página de inicio

--- src/pages/BienvenidaRV.vue
+++ src/pages/BienvenidaRV.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="BienvenidoUsuario.jpeg" style="width: 500px; height: 500px" />
+    <img alt="Bienvenido" src="/BienvenidoUsuario.jpeg" style="width: 500px; height: 500px" />
   </q-page>
 </template>
 
